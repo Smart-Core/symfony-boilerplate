57ffba18b4bfe7c679d0e4f22d1efef8341dcfe8
Smart-Core <artem.digi@gmail.com>
2016-10-08 00:35:38 +0700

    fixes

--- web/app.php
+++ web/app.php
@@ -1,11 +1,12 @@
 <?php
-define('START_TIME', microtime(true));
-define('START_MEMORY', memory_get_usage());
-define('DIR_WEB', getcwd());
 define('APPKERNEL_DEBUG', true);
 //define('APPKERNEL_DEBUG', false);
 
-require_once __DIR__.'/../app/bootstrap.php.cache';
+define('START_TIME', microtime(true));
+define('START_MEMORY', memory_get_usage());
+define('DIR_WEB', getcwd());
+
+require_once __DIR__.'/../var/bootstrap.php.cache';
 require_once __DIR__.'/../app/AppKernel.php';
 //require_once __DIR__.'/../app/AppCache.php';
 
