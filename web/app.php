535a013a5128a73938f18151323dabeb90f6cd2f
Smart-Core <artem.digi@gmail.com>
2017-01-30 00:25:40 +0700

    upd

--- web/app.php
+++ web/app.php
@@ -4,9 +4,12 @@ define('APPKERNEL_DEBUG', true);
 
 use Symfony\Component\HttpFoundation\Request;
 
+if (APPKERNEL_DEBUG === false) {
+    include_once __DIR__.'/../var/bootstrap.php.cache';
+}
+
 /** @var \Composer\Autoload\ClassLoader $loader */
 $loader = require __DIR__.'/../app/autoload.php';
-include_once __DIR__.'/../var/bootstrap.php.cache';
 
 $kernel = new AppKernel('prod', APPKERNEL_DEBUG);
 $kernel->loadClassCache();
